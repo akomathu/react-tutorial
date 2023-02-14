9085fbd489f0a2a246c8753190a7aaada9beb231
akomathu <pc-504@st-ove.co.jp>
2023-02-14 14:53:50 +0900

    todo

--- src/todo.js
+++ src/todo.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
-const todo = ({ todo }) => {
-  return <div>{todo.name}</div>
+const todo = ({ todo, toggleTodo }) => {
+
+  const handleTodoClick = () => {
+    toggleTodo(todo.id);
+  };
+  return <div>
+    <label>
+      <input 
+      type="checkbox" 
+      checked={todo.completed} 
+      readOnly 
+      onChange={handleTodoClick} />
+    </label>
+    {todo.name}</div>
 };
 
 export default todo
